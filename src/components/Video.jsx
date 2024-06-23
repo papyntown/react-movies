d5cb75bc49a0dc4f2d5a942a60c0f244806b921f
papyntown <realistedem@gmail.com>
2024-06-23 20:09:44 +0200

    with import

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,11 @@
 import React from "react";
+import backgroundNoel from "../assets/img/backgroundNoel.png";
 
 const Video = () => {
     return (
         <div className="content-container">
             <div className="video-container">
-                <img
-                    id="myVideo"
-                    src="./src/assets/img/backgroundNoel.png"
-                    alt="bg-noel"
-                />
+                <img id="myVideo" src={backgroundNoel} alt="bg-noel" />
                 {/* <video
                     src="./src/assets/decorations-de-noel-illuminees.mp4"
                     autoPlay
